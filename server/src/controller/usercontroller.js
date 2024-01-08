58dccdfe1df271a621ee7eeb745506a45e843cbf
BumbleBee09 <vrajdesai67@gmail.com>
2024-01-08 23:34:30 +0530

    completing the user controllers

---
+++ server/src/controller/usercontroller.js
@@ -0,0 +1,35 @@
+const userService = require("../services/user_service");
+
+const getUSerProfile = async(req,res) => {
+
+    try {
+        const jwt = req.headers.authorization?.split(" ")[1];
+
+        if(!jwt){
+            return res.status(404).send({error:"Token not found"})
+        }
+
+        const user = await userService.getUserProfileByToken(jwt);
+
+        return re.status(200).send(user);
+
+    } catch (error) {
+        
+        return res.status(500).send({error: error.message})
+    }
+
+}
+
+const getAllUsers = async(req,res) => {
+    try {
+        
+        const users = await userService.getAllUsers();
+
+        return res.status(200).send(users);
+    } catch (error) {
+        
+        return res.status(500).send({error: error.message});
+    }
+}
+
+module.exports = {getUSerProfile, getAllUsers};
