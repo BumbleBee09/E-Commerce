a247d232360ae6bf70557120fc1a4bf0d0a805f1
BumbleBee09 <vrajdesai67@gmail.com>
2024-01-08 23:16:18 +0530

    Adding the auth controllers desgined for login and register

---
+++ server/src/controller/authcontroller.js
@@ -0,0 +1,48 @@
+const userService = require("../services/user_service");
+
+const jwtProvider = require("../config/jwtProvider");
+const User = require("../models/user_model");
+const bcrypt = require('bcrypt');
+
+const register = async(req,res) => {
+    try {
+        const user = await userService.createUser(req.body);
+        
+        const jwt = jwtProvider.generateToken(user._id);
+
+        // creating the cart after successful registration becasue after the registration this is the only way to create cart
+        // await cartService.createCart(user);
+
+        return res.status(200).send({jwt, message:"Successfully Registration"})
+    } catch (error) {
+        return res.status(500).send({error: error.message});
+    }
+}
+
+const login = async(req,res) => {
+
+    const {password, email} = req.body;
+    try {
+
+        const user = await userService.findUserByEmail(email);
+
+        if(!user){
+            return res.status(404).send({message:"User not found with email",email});
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if(!isPasswordValid){
+            return res.status(401).send({message:"Invalid Password...."})
+        }
+
+        const jwt = jwtProvider.generateToken(user._id);
+
+        return res.status(200).send({jwt, message:"Successful Login"});
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {register, login}
