84349281494ccb1e18a1e9f658372fb18648a10a
BumbleBee09 <vrajdesai67@gmail.com>
2024-01-17 18:48:52 +0530

    Adding new services in the cart_services

--- server/src/services/cart_service.js
+++ server/src/services/cart_service.js
@@ -1,4 +1,6 @@
 const Cart = require("../models/cart_model");
+const CartItem = require("../models/cartItem_model");
+const Product = require("../models/product_model");
 
 async function createCart(user) {
     try {
@@ -12,4 +14,70 @@ async function createCart(user) {
     }
 }
 
-module.exports = { createCart };
+const findUserCart = async(userId) => {
+
+    try {
+
+        let cart = await Cart.findOne({user: userId})
+
+        let cartItems = await CartItem.find({cart: cart._id}).populate("product");
+
+        cart.cartItems = cartItems;
+
+        let totalPrice = 0;
+        let totalDiscountedPrice = 0;
+        let totalItem = 0;
+
+        for(let cartItem of cart.cartItems){
+            totalPrice += cartItem.price;
+            totalDiscountedPrice += cartItem.discountedPrice;
+            totalItem += cartItem.quantity;
+        }
+
+        cart.totalPrice = totalPrice;
+        cart.totalItem = totalItem;
+        cart.discount = totalPrice - totalDiscountedPrice;
+
+        return cart;
+        
+    } catch (error) {
+
+        throw new Error(`error in the findUserCart: ${error.message}`);
+        
+    }
+}
+
+const addCartItem = async(userId, req) => {
+    try {
+
+        const cart = await Cart.findOne({user: userId});
+        const product = await Product.findById(req.productId);
+
+        const isPresent = await CartItem.findOne({cart: cart._id, product: product._id, userId})
+
+        if(!isPresent){
+
+            const cartItem = new CartItem({
+                product: product._id,
+                cart: cart._id,
+                quantity:1,
+                userId,
+                price:product.price,
+                size: req.size,
+                discountedPrice: product.discountedPrice
+            })
+
+            const createdCartItem = await cartItem.save();
+
+            cart.cartItems.push(createdCartItem);
+            await cart.save();
+
+            return "Item added to cart successfullly";
+        }
+        
+    } catch (error) {
+        throw new Error(`error in the addCartItem: ${error.message}`);
+    }
+}
+
+module.exports = { createCart, findUserCart, addCartItem };
