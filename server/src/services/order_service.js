105aecad80835e14804e29cc46d8d02c994a42a2
BumbleBee09 <vrajdesai67@gmail.com>
2024-02-03 11:41:12 +0530

    adding order services

--- server/src/services/order_service.js
+++ server/src/services/order_service.js
@@ -1,23 +1,149 @@
-const cartService = require("../services/cart_service")
+const cartService = require("../services/cart_service");
 
 const Address = require("../models/address_model");
 
-const createOrder = async(user, shipAddress) => {
-    let address;
+const Order = require("../models/order_model");
 
-    if(shipAddress._id){
-        let existAddress = await Address.findById(shipAddress._id);
+const createOrder = async (user, shipAddress) => {
+  let address;
 
-        address = existAddress;
-    }
-    else{
-        address = new Address(shipAddress);
-        address.user = user;
-        await address.save();
+  if (shipAddress._id) {
+    let existAddress = await Address.findById(shipAddress._id);
+    address = existAddress;
+  } else {
+    address = new Address(shipAddress);
+    address.user = user;
+    await address.save();
 
-        user.addresses.push(address);
-        await user.save(); 
-    }
+    user.addresses.push(address);
+    await user.save();
+  }
 
-    
-}
+  const cart = await cartService.findUserCart(user._id);
+  const orderItems = [];
+
+  for (const item of cart.cartItems) {
+    const orderItem = new orderItems({
+      price: item.price,
+      product: item.product,
+      quantity: item.quantity,
+      size: item.size,
+      userId: item.userId,
+      discountedPrice: item.discountedPrice,
+    });
+
+    const createdOrderItem = await orderItem.save();
+    orderItems.push(createdOrderItem);
+  }
+
+  const createdOrder = new Order({
+    user,
+    orderItems,
+    totalPrice: cart.totalPrice,
+    totalDiscountedPrice: cart.totalDiscountedPrice,
+    discount: cart.discount,
+    totalItem: cart.totalItem,
+    shippingAddress: address,
+  });
+
+  const savedOrder = await createdOrder.save();
+
+  return savedOrder;
+};
+
+const placeOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+
+  order.orderStatus = "PLACED";
+  order.paymentDetails.paymentStatus = "COMPLETED";
+
+  return await order.save();
+};
+
+const confirmOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+
+  order.orderStatus = "CONFIRMED";
+
+  return await order.save();
+};
+
+const shipOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+
+  order.orderStatus = "SHIPPED";
+
+  return await order.save();
+};
+
+const deliverOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+
+  order.orderStatus = "DELIVERED";
+
+  return await order.save();
+};
+
+const cancelOrder = async (orderId) => {
+  const order = await findOrderById(orderId);
+
+  order.orderStatus = "CANCELLED";
+
+  return await order.save();
+};
+
+const findOrderById = async (orderId) => {
+  const order = await Order.findById(orderId)
+    .populate("user")
+    .populate({ path: "orderItems", populate: { path: "product" } })
+    .populate("shippingAddress");
+
+  return order;
+};
+
+const userOrderHistory = async (userId) => {
+  try {
+    const orders = await Order.find({ user: userId, orderStatus: "PLACED" })
+      .populate({ path: "orderItems", populate: { path: "product" } })
+      .lean();
+
+    return orders;
+  } catch (error) {
+    throw new Error("Error in order history", error.message);
+  }
+};
+
+const getAllOrder = async () => {
+  try {
+    const orders = await Order.find()
+      .populate({ path: "orderItems", populate: { path: "product" } })
+      .lean();
+
+    return orders;
+  } catch (error) {
+    throw new Error("Error in get all order", error.message);
+  }
+};
+
+const deleteOrder = async (orderId) => {
+  try {
+    const order = await findOrderById(orderId);
+
+    await Order.findByIdAndDelete(order._id);
+  } catch (error) {
+    throw new Error("Error in delete order", error.message);
+  }
+};
+
+module.exports = {
+  createOrder,
+  placeOrder,
+  confirmOrder,
+  shipOrder,
+  deliverOrder,
+  cancelOrder,
+  findOrderById,
+  userOrderHistory,
+  getAllOrder,
+  deleteOrder,
+};
