d610a43ecb3c81a953d96dcfb52cc1f4afe6078a
BumbleBee09 <vrajdesai67@gmail.com>
2024-02-05 19:52:53 +0530

    implementing review and rating services

---
+++ server/src/services/rating_service.js
@@ -0,0 +1,27 @@
+const Rating = require("../models/rating_model");
+
+const productService = require("../services/product_service");
+
+const createRating = async(reqData, user) => {
+    const product = await productService.findProductById(reqData.productId);
+
+    const rating = new Rating({
+        user : user._id,
+        product: product._id,
+        rating : reqData.rating,
+        createdAt: new Date(),
+    })
+
+    await product.save();
+
+    return await rating.save();
+}
+
+const getProductRating = async(productId) => {
+    return await Rating.find({product: productId});
+}
+
+module.exports = {
+    createRating,
+    getProductRating,
+}
