a1465fc1c03084d046e5d287a39ddd9c5d98647e
BumbleBee09 <vrajdesai67@gmail.com>
2024-02-05 22:10:35 +0530

    Making a middleware for authetication purpose for controllers

---
+++ server/src/middleware/authenticate.js
@@ -0,0 +1,24 @@
+const jwtProvider = require("../config/jwtProvider");
+const userService = require("../services/user_service");
+
+const authenticate = async(req,res) => {
+    try {
+        const token = req.headers.authorization?.split(" ")[1];
+
+        if(!token){
+            return req.status(404).send({error:"Token not found...."})
+        }
+
+        const userId = jwtProvider.getUserIdFromToken(token);
+
+        const user = userService.findUserById(userId);
+
+        req.user = user;
+    } catch (error) {
+        return res.status(500).send({error:error.message});
+    }
+
+    next();
+}
+
+module.exports = authenticate;
