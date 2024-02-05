c807f2e2eea75b81922dd78dbbcb6409f94b935f
BumbleBee09 <vrajdesai67@gmail.com>
2024-02-05 23:07:08 +0530

    Regrestering all the apis into the index.js file

--- server/src/index.js
+++ server/src/index.js
@@ -16,4 +16,28 @@ app.use("/auth", authRouters);
 const userRouters= require("./routes/user_route.js");
 app.use("/api/users", userRouters);
 
+const userProductRoutes = require("./routes/userProductRoutes.js");
+app.use("/api/products", userProductRoutes);
+
+const adminProductRoutes = require("./routes/adminProductRoutes.js");
+app.use("/api/products", adminProductRoutes);
+
+const cartRouter = require("./routes/cart_route.js");
+app.use("/api/cart", cartRouter);
+
+const cartItemRouter = require("./routes/cartItem_route.js");
+app.use("/api/cart_item", cartItemRouter);
+
+const userOrderRouter = require("./routes/userOrderRoutes.js");
+app.use("/api/orders", userOrderRouter);
+
+const adminOrderRoutes = require("./routes/adminOrderRoutes.js");
+app.use("/api/admin/orders", adminOrderRoutes);
+
+const reviewRouter = require("./routes/review_route.js");
+app.use("/api/reviews", reviewRouter);
+
+const ratingRouter = require("./routes/rating_route.js");
+app.use("/api/ratings", ratingRouter);
+
 module.exports = app;
